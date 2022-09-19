d67814484a95d64fcf634a8cb972ea3421465da4
khizarsiddiqui <khizarsiddiqui10@gmail.com>
2022-09-19 19:56:38 +0500

    Adding CLI options and art strings writing to a new file

--- file-10-ascii.py
+++ file-10-ascii.py
@@ -42,3 +42,50 @@ def getAverageL(image):
 # Generating the ASCII Content from the Image (step 3)
 # an ASCII image is a list of character strings
 aimg = []
+# generate the list of tile dimensions
+for j in range(rows):
+# Although these are floating-point calculations, 
+# truncate them to integers before passing them to an image-cropping method.
+    y1 = int(j*h)
+    y2 = int((j+1)*h)
+# correct the last tile
+    if j == rows-1:
+        y2 = H
+# append an empty string
+    aimg.append("")
+    for i in range(cols):
+# crop the image to fit the tile
+        x1 = int(i*w)
+        x2 = int((i+1)*w)
+# correct the last tile
+        if i == cols-1:
+            x2 = W
+# crop the image to extract the tile into another Image object
+        img = image.crop((x1, y1, x2, y2))
+# get the average luminance
+        avg = int(getAverageL(img))
+# look up the ASCII character for grayscale value (avg)
+        if moreLevels:
+            gsval = gscale1[int((avg*69)/255)]
+        else:
+            gsval = gscale2[int((avg*9)/255)]
+# append the ASCII character to the string
+        aimg[j] += gsval
+
+# Command line operators (step 4)
+parser = argparse.ArgumentParser(description="descStr")
+# add expected arguments
+parser.add_argument('--file', dest='imgFile', required=True)
+parser.add_argument('--scale', dest='scale', required=False)
+parser.add_argument('--out', dest='outFile', required=False)
+parser.add_argument('--cols', dest='cols', required=False)
+parser.add_argument('--morelevels', dest='moreLevels', action='store_true')
+
+# Writing the ASCII Art Strings to a Text File (step 5)
+# open a new text file
+f = open(outFile, 'w')
+# write each string in the list to the new file
+for row in aimg:
+    f.write(row + '\n')
+# clean up
+f.close() 
