116ed732598a8260d92efb6bdbee5062ad9344b0
khizarsiddiqui <khizarsiddiqui10@gmail.com>
2022-09-20 02:25:16 +0500

    Adding CLI options and art strings writing to a new file(2)

--- file-10-ascii.py
+++ file-10-ascii.py
@@ -12,23 +12,13 @@ from PIL import Image
 # Defining the Grayscale Levels and Grid (step 1)
 # a ramp (an increasing set of values) of ASCII characters to represent grayscale values in the range [0, 255].
 
-cols=80
-scale=0.43
+# grayscale level values from:
+# http://paulbourke.net/dataformats/asciiart/
 
 # 70 levels of gray
 gscale1 = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`". "
 # 10 levels of gray
 gscale2 = "@%#*+=-:. "
-# open the image and convert to grayscale
-image = Image.open(fileName).convert("L")
-# store the image dimensions
-W, H = image.size[0], image.size[1]
-# compute the tile width
-w = W/cols
-# compute the tile height based on the aspect ratio and scale of the font
-h = w/scale
-# compute the number of rows to use in the final grid
-rows = int(H/h)
 
 # Computing the Average Brightness (step 2)
 def getAverageL(image):
@@ -39,39 +29,61 @@ def getAverageL(image):
 # get the average
     return np.average(im.reshape(w*h))
 
+def covertImageToAscii(fileName, cols, scale, moreLevels):
+    # declare globals
+    global gscale1, gscale2
+# open the image and convert to grayscale
+    image = Image.open(fileName).convert("L")
+# store the image dimensions
+    W, H = image.size[0], image.size[1]
+# compute the tile width
+    w = W/cols
+# compute the tile height based on the aspect ratio and scale of the font
+    h = w/scale
+# compute the number of rows to use in the final grid
+    rows = int(H/h)
+    print("cols: %d, rows: %d" % (cols, rows))
+    print("tile dims: %d x %d" % (w, h))
+# check if image size is too small
+    if cols > W or rows > H:
+        print("Image too small for specified cols!")
+        exit(0)
 # Generating the ASCII Content from the Image (step 3)
 # an ASCII image is a list of character strings
-aimg = []
+    aimg = []
 # generate the list of tile dimensions
-for j in range(rows):
-# Although these are floating-point calculations, 
-# truncate them to integers before passing them to an image-cropping method.
-    y1 = int(j*h)
-    y2 = int((j+1)*h)
-# correct the last tile
-    if j == rows-1:
-        y2 = H
+    for j in range(rows):
+        y1 = int(j*h)
+        y2 = int((j+1)*h)
+        # correct the last tile
+        if j == rows-1:
+            y2 = H
 # append an empty string
-    aimg.append("")
-    for i in range(cols):
+        aimg.append("")
+        for i in range(cols):
 # crop the image to fit the tile
-        x1 = int(i*w)
-        x2 = int((i+1)*w)
+            x1 = int(i*w)
+            x2 = int((i+1)*w)
 # correct the last tile
-        if i == cols-1:
-            x2 = W
+            if i == cols-1:
+                x2 = W
 # crop the image to extract the tile into another Image object
-        img = image.crop((x1, y1, x2, y2))
+            img = image.crop((x1, y1, x2, y2))
 # get the average luminance
-        avg = int(getAverageL(img))
+            avg = int(getAverageL(img))
 # look up the ASCII character for grayscale value (avg)
-        if moreLevels:
-            gsval = gscale1[int((avg*69)/255)]
-        else:
-            gsval = gscale2[int((avg*9)/255)]
+            if moreLevels:
+                gsval = gscale1[int((avg*69)/255)]
+            else:
+                gsval = gscale2[int((avg*9)/255)]
 # append the ASCII character to the string
         aimg[j] += gsval
 
+# return text image
+    return aimg
+
+
+# main function
 # Command line operators (step 4)
 parser = argparse.ArgumentParser(description="descStr")
 # add expected arguments
