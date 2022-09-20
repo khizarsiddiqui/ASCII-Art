bcdd908eb67e1708e6ce5217339864ccd0b39a80
khizarsiddiqui <khizarsiddiqui10@gmail.com>
2022-09-21 02:00:29 +0500

    Finalized

--- file-10-ascii.py
+++ file-10-ascii.py
@@ -8,6 +8,7 @@ from multiprocessing.util import close_all_fds_except
 import sys, random, argparse
 import numpy as np
 import math
+
 from PIL import Image
 # Defining the Grayscale Levels and Grid (step 1)
 # a ramp (an increasing set of values) of ASCII characters to represent grayscale values in the range [0, 255].
@@ -16,7 +17,7 @@ from PIL import Image
 # http://paulbourke.net/dataformats/asciiart/
 
 # 70 levels of gray
-gscale1 = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`". "
+gscale1 = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'."
 # 10 levels of gray
 gscale2 = "@%#*+=-:. "
 
@@ -29,7 +30,7 @@ def getAverageL(image):
 # get the average
     return np.average(im.reshape(w*h))
 
-def covertImageToAscii(fileName, cols, scale, moreLevels):
+def convertImageToAscii(fileName, cols, scale, moreLevels):
     # declare globals
     global gscale1, gscale2
 # open the image and convert to grayscale
@@ -82,22 +83,48 @@ def covertImageToAscii(fileName, cols, scale, moreLevels):
 # return text image
     return aimg
 
-
 # main function
+def main():
 # Command line operators (step 4)
-parser = argparse.ArgumentParser(description="descStr")
+    descStr = "This program converts an image into ASCII Art."
+    parser = argparse.ArgumentParser(description="descStr")
 # add expected arguments
-parser.add_argument('--file', dest='imgFile', required=True)
-parser.add_argument('--scale', dest='scale', required=False)
-parser.add_argument('--out', dest='outFile', required=False)
-parser.add_argument('--cols', dest='cols', required=False)
-parser.add_argument('--morelevels', dest='moreLevels', action='store_true')
+    parser.add_argument('--file', dest='imgFile', required=True)
+    parser.add_argument('--scale', dest='scale', required=False)
+    parser.add_argument('--out', dest='outFile', required=False)
+    parser.add_argument('--cols', dest='cols', required=False)
+    parser.add_argument('--morelevels', dest='moreLevels', action='store_true')
 
+# parse arguments
+    args = parser.parse_args()
+
+    imgFile = args.imgFile
+# set output file
+    outFile = "out.txt"
+    if args.outFile:
+        outFile = args.outFile
+# set scale default as 0.43, which suits a Courier font
+    scale = 0.43
+    if args.scale:
+        scale = float(args.scale)
+# set cols
+    cols = 80
+    if args.cols:
+        cols = int(args.cols)
+        
+    print('generating ASCII art ...')
+# convert image to ASCII text
+    aimg = convertImageToAscii(imgFile, cols, scale, args.moreLevels)
 # Writing the ASCII Art Strings to a Text File (step 5)
 # open a new text file
-f = open(outFile, 'w')
+    f = open(outFile, 'w')
 # write each string in the list to the new file
-for row in aimg:
-    f.write(row + '\n')
+    for row in aimg:
+        f.write(row + '\n')
 # clean up
-f.close() 
+    f.close()
+    print("ASCII art written to %s" % outFile)
+
+# call main
+if __name__ == '__main__':
+    main() 
